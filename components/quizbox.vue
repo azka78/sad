9c1287f680930bb1beebb8cf5816ac3a366ef8aa
chinbat <chinbat.carl@gmail.com>
2020-02-01 01:58:45 +0800

    “blah2”

---
+++ components/quizbox.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="new">
+
+  </section>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+  export default {
+
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+    xs12
+    sm8
+    md6
+    >
+
+    </v-flex>
+    <login
+    xs6
+    sm8
+    md6
+    v-model="login"
+    fixed
+    />
+    <button type="button" name="button" @click.stop="login = !login"></button>
+
+  </v-layout>
+</template>
+
+<script>
+  import login from '~/components/login.vue'
+  export default {
+    components: {
+      login
+    },
+    data () {
+      return {
+        login: false
+      }
+    }
+  }
+
+
+  </script>
